6e8ae9ccb2d564a787c8a17765e90e6c75acddfb
Rodrigo <arcanjomitsuo@gmail.com>
2021-07-13 21:47:01 -0300

    Gerando sessão de checkout

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
-import { query as q } from "faunadb";
-import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
-import { fauna } from "../../../services/fauna";
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: "read:user",
+      scope: 'read:user',
     }),
   ],
 
@@ -21,11 +21,11 @@ export default NextAuth({
           q.If(
             q.Not(
               q.Exists(
-                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email))
               )
             ),
-            q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email)))
           )
         );
 
